4faa284616157ed042e9751a231b95f71ec4f234
avetisyanmarine <marineavet2006@gmail.com>
2026-04-21 17:53:14 +0400

    rtyuio

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react"
-import { FirstPage } from "./components/first-page"
-import { ForthPage } from "./components/forth-page"
-import { LastFooterPage } from "./components/last-footer-page"
-import { MusicPage } from "./components/music-page"
-import { SecondPage } from "./components/second-page"
-import { ThirdPage } from "./components/third-page"
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { BackToTop } from "./components/top-button"
+import { useEffect } from "react";
+import { FirstPage } from "./components/first-page";
+import { ForthPage } from "./components/forth-page";
+import { LastFooterPage } from "./components/last-footer-page";
+import { MusicPage } from "./components/music-page";
+import { SecondPage } from "./components/second-page";
+import { ThirdPage } from "./components/third-page";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { BackToTop } from "./components/top-button";
+import AttendanceGuests from "./components/AttendanceGuests";
 
 function App() {
   useEffect(() => {
     AOS.init({ duration: 700, once: true });
-
   }, []);
 
   return (
@@ -22,10 +22,11 @@ function App() {
       <SecondPage />
       <ThirdPage />
       <ForthPage />
+      <AttendanceGuests />
       <LastFooterPage />
       <BackToTop />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
